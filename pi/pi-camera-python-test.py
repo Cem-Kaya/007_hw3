75e7de6bbddbdfeab1f63ff4860a854df3dee6f5
Cem-Kaya <cemkaya2704@gmail.com>
2022-12-01 20:58:08 +0300

    first integration test

--- pi/pi-camera-python-test.py
+++ pi/pi-camera-python-test.py
@@ -4,20 +4,37 @@
 # capture to a file, the return value is the metadata for that image.
 
 import time
-
+import requests as re
+import json 
+import base64
 from picamera2 import Picamera2, Preview
+import yaml
+
+with open("conf.yaml", "r") as stream:
+    try:
+        ips =yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+    
+print(ips)
+
+ip = "172.22.169.87"
 
 picam2 = Picamera2()
 
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+preview_config = picam2.create_preview_configuration(main={"size": (1920, 1080)})
 picam2.configure(preview_config)
 
 picam2.start_preview(Preview.QTGL)
 
 picam2.start()
 time.sleep(2)
+while True:    
+    jpeg_buffer = picam2.capture_buffer()
+    data = {}
+    data['img'] = base64.encodebytes(jpeg_buffer).decode('utf-8')
+    
+    re.post("http://ip:5000", data )
 
-metadata = picam2.capture_file("test.jpg")
-print(metadata)
 
 picam2.close()
--- server/back_end.py
+++ server/back_end.py
@@ -6,7 +6,9 @@ import requests as req
 from flask_cors import CORS
 from flask import Flask, render_template, request, send_from_directory, send_file
 import torch 
-
+import torchvision
+import cv2
+import base64
 # py -3.10 .\back_end.py # to run the server
 # py -3.10 -m pip freeze > requirements.txt # to create requirements.txt
 # py -3.10 -m pip install -r requirements.txt # to install requirements.txt 
@@ -23,8 +25,12 @@ def index():
 
 @app.route('/post_test', methods=['POST'], strict_slashes=False  )
 def test():
-  myobj = {'test': request.form['test']  }
-  return render_template("success.html", data= req.post(data = json.dumps(myobj)).text )    
+    myobj = {'img': request.form['img']  }
+    print (myobj )
+    image = base64.decodestring(json.dumps(data)['image'])
+    print (image)
+    print(myobj['img'] .dtype )
+    return "test" #render_template("success.html", data= req.post(data = json.dumps(myobj)).text )    
 
 
 
@@ -41,5 +47,5 @@ if __name__ == '__main__':  #python interpreter assigns "__main__" to the file y
     results = yolo_model(imgs)
     # Results
     results.print()
-    results.show()  # or .show()
+    #results.show()  # or .show()
     app.run(debug=True)
