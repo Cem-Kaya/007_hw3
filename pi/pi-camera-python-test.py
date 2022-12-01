f4fe24144d6074251e6f9232f36882e0ec187aec
Cem-Kaya <cemkaya2704@gmail.com>
2022-12-01 22:23:08 +0300

    first integration test14

--- pi/pi-camera-python-test.py
+++ pi/pi-camera-python-test.py
@@ -32,10 +32,9 @@ time.sleep(2)
 while True:    
     picam2.capture_file("tmp.jpg")
     time.sleep(0.2) 
-    try:    
-        print( re.post("http://"+ip+":5000/post_test", files={'image': open('tmp.jpg', 'rb')}) )    
-    except:
-        print("Network Error") 
+      
+    print( re.post("http://"+ip+":5000/post_test", files={'image': open('tmp.jpg', 'rb')}) )    
+   
     
 
 
