315b7b0ca2bc17aba644e3b410f49a23c19965a9
emre <eminc2033@gmail.com>
2022-12-23 00:06:25 +0300

    BCM

--- pi/dumm.py
+++ pi/dumm.py
@@ -8,7 +8,7 @@ chunk = 4096 # 2^12 samples for buffer
 record_secs = 6 # seconds to record
 dev_index = 2 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = 'test1.wav' # name of .wav file
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 ip = "172.22.5.242"
 try:
